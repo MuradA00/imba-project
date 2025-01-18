7568867ed91e8ea59738f71b29c0dad647ca53db
MuradA00 <murad.abyshly@cowchain.io>
2025-01-18 03:27:53 +0200

    99%

--- source/js/main.js
+++ source/js/main.js
@@ -4,27 +4,10 @@ import './_vendor';
 
 
 const menuButton = document.querySelector('.header-menu');
-const menu = document.querySelector('.header-body_desktop');
-const tabsControllers = document.querySelectorAll(".news-filter__btn");
-const tabsContents = document.querySelectorAll(".news-content");
+const menu = document.querySelector('.nav');
 const closeMenuButton = document.querySelector('.header-close')
 const body = document.body;
 
-if (tabsControllers.length) {
-  tabsControllers.forEach((controller, i) => {
-    controller.addEventListener("click", () => {
-      if (controller.classList.contains("button--purple")) return;
-
-      tabsControllers.forEach(
-        controller => controller.classList.remove("button--purple")
-      );
-      tabsContents.forEach(content => content.classList.remove("fade-in"));
-
-      controller.classList.add("button--purple");
-      tabsContents[i].classList.add("fade-in");
-    })
-  })
-}
 
 // const closeModalByOuterClick = (modal) => {
 //   const modalContainer = modal.querySelector('.modal-container');
@@ -47,14 +30,23 @@ if (tabsControllers.length) {
 //   document.documentElement.classList.add('locked');
 // }
 
+if (AOS) {
+  AOS.init({
+    once: true,
+    offset: 150,
+    startEvent: "load",
+    duration: 500
+  });
+}
+
 const menuHandler = () => {
   menuButton.classList.toggle('header-menu--active');
   if (menuButton.classList.contains('header-menu--active')) {
-    menu.classList.add('header-body_desktop--active')
+    menu.classList.add('nav--active')
     document.documentElement.style.overflow = 'hidden';
     body.style.overflow = 'hidden'
   } else {
-    menu.classList.remove('header-body_desktop--active')
+    menu.classList.remove('nav--active')
     document.documentElement.style.overflow = '';
     body.style.overflow = ''
   }
@@ -65,7 +57,7 @@ if (menuButton) {
 
   closeMenuButton.addEventListener('click', () => {
     menuButton.classList.remove('header-menu--active');
-    menu.classList.remove('header-body_desktop--active')
+    menu.classList.remove('nav--active')
     document.documentElement.style.overflow = '';
     body.style.overflow = ''
   })
@@ -73,29 +65,23 @@ if (menuButton) {
 
 
 if (Swiper) {
-  const newsSliderProps = {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    breakpoints: {
-      500: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-      },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 40,
-      },
-      1200: {
-        slidesPerView: 4,
-        spaceBetween: 50,
-      }
+  new Swiper(".slider-body", {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    pagination: {
+      el: '.slider-pagination',
+      type: 'bullets',
     },
-  }
+  })
 
   new Swiper(".about-main", {
     speed: 500,
     slidesPerView: "auto",
     spaceBetween: 0,
+    pagination: {
+      el: '.about-pagination',
+      type: 'bullets',
+    },
   })
 
   new Swiper('.faq-inner', {
@@ -119,30 +105,6 @@ if (Swiper) {
     navigation: {
       nextEl: ".faq-controls__btn--next",
       prevEl: ".faq-controls__btn--prev"
-    }
-  })
-
-  new Swiper(".news-content--all .news-slider", {
-    ...newsSliderProps,
-    navigation: {
-      nextEl: ".news-content--all .news-controls__btn--next",
-      prevEl: ".news-content--all .news-controls__btn--prev"
-    }
-  })
-
-  new Swiper(".news-content--news .news-slider", {
-    ...newsSliderProps,
-    navigation: {
-      nextEl: ".news-content--news .news-controls__btn--next",
-      prevEl: ".news-content--news .news-controls__btn--prev"
-    }
-  })
-
-  new Swiper(".news-content--events .news-slider", {
-    ...newsSliderProps,
-    navigation: {
-      nextEl: ".news-content--events .news-controls__btn--next",
-      prevEl: ".news-content--events .news-controls__btn--prev"
     }
   })
 }
